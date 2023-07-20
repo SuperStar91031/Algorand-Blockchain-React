79f4dcb90a72d3e8399d3ad716df906d9cbe8c07
superstar <117964815+SuperStar91031@users.noreply.github.com>
2023-07-20 15:11:11 +0200

    done

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+const ALGOD_SERVER = 'https://testnet-algorand.api.purestake.io/ps2'
+const INDEXER_SERVER = 'https://testnet-algorand.api.purestake.io/idx2'
+const TOKEN = { 'X-API-Key': '' }
+const PORT = '443';
+
+
+module.exports = {
+    ALGOD_SERVER,
+    INDEXER_SERVER,
+    TOKEN,
+    PORT,
+}
